1e763ede181dea64f32ca4b0b158747cfcb791e6
abiliokrismanuel <abilio.krismanuel@ti.ukdw.ac.id>
2023-02-03 01:04:04 +0700

    #1 install express,nodemon,typescript

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express,{Application,Request,Response,NextFunction} from "express";
+
+const app: Application = express();
+const port: Number = 3000;
+
+app.use("/", (req:Request ,res:Response ,next:NextFunction) => {
+    res.status(200).send({data: "Hello World"})
+
+})
+
+app.listen(port,() => console.log(`Server is listening on port ${port}`));
